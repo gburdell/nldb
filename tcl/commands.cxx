5f5d762f7ee5a56bab0f184e240832da58d9ba24
George P Burdell <gburdell77@gmail.com>
2014-12-08 09:53:13 -0800

    rekindle this effort starting with toplevel makefile. subproject xyzzy is moved to separate gitbub/xyzzy.

--- tcl/commands.cxx
+++ tcl/commands.cxx
@@ -63,7 +63,7 @@ namespace vnltcl {
     typedef PTArray<string> TStringAr;
     typedef PTRcArray<string> TRcStringAr;
 
-    static const string stToolVersion = "nl_shell v2012-07-26_16.20.00";
+    static const string stToolVersion = "nl_shell v2014-12-03_09.54.09";
     static const char *stPtrPfx = "_obj";
     static const string stNilArg = "_";
     // add 2 for 0x
